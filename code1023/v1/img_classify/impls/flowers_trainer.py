a6948ed31243e1b08a810f74e5a687847a1e6062
xing <52873782+LZX842056112@users.noreply.github.com>
2026-01-07 20:36:44 +0800

    code_1023

---
+++ code1023/v1/img_classify/impls/flowers_trainer.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Create Date Time : 2025/10/14 20:28
+Create User : 19410
+Desc : 17种花的模型训练器
+"""
+import os.path
+
+import torch
+from torch import nn
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import utils
+
+from ..data_utils import build_dataloader, DataAugmentArgs
+from ..looses import build_losses
+from ..network import build_network
+from ..optim_utils import build_optim_v2 as build_optim
+from ..optim_utils import OptimArgs
+from ..trainer import Trainer
+
+
+class FlowersTrainer(Trainer):
+    def __init__(self, train_path: str, val_path: str,
+                 batch_size: int, total_epoch: int, root_dir: str,
+                 img_target_size=(100, 100)):
+        # 1. 数据加载 数据处理
+        _, train_dataloader, class_names = build_dataloader(
+            data_dir=train_path, batch_size=batch_size, shuffle=True,
+            target_size=img_target_size,
+            is_augment=True,
+            aug_args=DataAugmentArgs(
+                p_rota=0.8
+            )
+        )
+        _, val_dataloader, _ = build_dataloader(
+            data_dir=val_path, batch_size=batch_size * 2, shuffle=False,
+            target_size=img_target_size,
+            class_names=class_names,
+            is_augment=False,
+        )
+
+        net = build_network(
+            num_classes=len(class_names),
+            in_channels=3
+        )
+        print(net)
+        loss_fn = build_losses()
+        train_opt = build_optim(
+            args=OptimArgs(
+                net=net,
+                lr=0.01
+            )
+        )
+
+        dump_model_dir = os.path.join(root_dir, "models")
+        summary_log_dir = os.path.join(root_dir, "logs")
+        super().__init__(
+            net=net,
+            loss_fn=loss_fn,
+            train_opt=train_opt,
+            train_dataloader=train_dataloader,
+            val_dataloader=val_dataloader,
+            total_epoch=total_epoch,
+            dump_model_dir=dump_model_dir,
+            class_names=class_names,
+            summary_writer=self.build_summary_writer(summary_log_dir, net)
+        )
+
+    def build_summary_writer(self, summary_log_dir, net):
+        # PS: 如果环境中默认有tensorflow的环境的，那么默认应该会自带tensorboard，可以不安装下列版本，直接使用默认的
+        # 安装tensorboard库  pip install tensorboard==2.13.0
+        # 如果运行过程中，出现缺少包或者其他依赖库相关异常，那么直接进行新的安装即可: pip install six==1.16.0 protobuf==3.20.0
+        # pip install six==1.16.0 protobuf==3.20.0 tensorboard==2.13.0
+        # 运行完成后/运行过程中，在命令行，输入以下命令:
+        # tensorboard --logdir xxx
+        # tensorboard --logdir D:\workspaces\study\CV202502\classify_17flowers\v1\output\logs
+        writer = SummaryWriter(log_dir=summary_log_dir)
+
+        def build_hook_func(that, module_name):
+            def hook_func(m, m_args, m_outputs):
+                tag = f"{'train' if m.training else 'eval'}_{module_name}"
+                _steps = {}
+                if hasattr(that, 'summary_steps'):
+                    _steps = getattr(that, 'summary_steps')
+                step = _steps.get(tag) or 1
+                _steps[tag] = step + 1
+                if step % 100 != 0:
+                    return
+
+                show_imgs = m_outputs[0:1]  # 仅对当前批次的第一张图像进行可视化 [1,C,H,W]
+                show_imgs = torch.transpose(show_imgs, 0, 1)  # [C,1,H,W] 将每个通道看成一个图像
+                # 多个通道合并成一个图像
+                grid = utils.make_grid(show_imgs, pad_value=0.5, normalize=True)
+                ndarr = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).detach().to("cpu", torch.uint8).numpy()
+
+                # 图像可视化输出
+                writer.add_image(
+                    tag, ndarr, global_step=step, dataformats='HWC'
+                )
+
+                return None
+
+            return hook_func
+
+        # 注册hooks
+        hook_handles = []
+        for name, module in net.named_modules():
+            if isinstance(module, nn.Conv2d):
+                _hook_fn = build_hook_func(
+                    that=self,
+                    module_name=f"{name}_conv"
+                )
+                handle = module.register_forward_hook(hook=_hook_fn)
+                hook_handles.append(handle)
+            elif isinstance(module, (nn.MaxPool2d, nn.AvgPool2d, nn.AdaptiveMaxPool2d, nn.AdaptiveAvgPool2d)):
+                _hook_fn = build_hook_func(
+                    that=self,
+                    module_name=f"{name}_pool"
+                )
+                handle = module.register_forward_hook(hook=_hook_fn)
+                hook_handles.append(handle)
+            elif isinstance(module, nn.BatchNorm2d):
+                _hook_fn = build_hook_func(
+                    that=self,
+                    module_name=f"{name}_bn"
+                )
+                handle = module.register_forward_hook(hook=_hook_fn)
+                hook_handles.append(handle)
+
+        # 在writer中增加一个属性
+        writer.hook_handles = hook_handles
+
+        return writer
---
+++ code1023/v1/img_classify/network.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Create Date Time : 2025/10/14 20:03
+Create User : 19410
+Desc : 定义网络相关的代码结构
+"""
+
+import torch.nn as nn
+from torchvision import models
+
+
+class ImageClassifyNetwork(nn.Module):
+    def __init__(self, num_classes, in_channels=3):
+        """
+        模型结构的初始化：主要负责定义模型结构中涉及到的模块对象
+        """
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 32, 3, 1, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(32, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool2d(8)
+        )
+
+        self.classify = nn.Linear(in_features=64 * 8 * 8, out_features=num_classes)
+
+    def forward(self, x):
+        """
+        前向执行方法
+        :param x: [N,C,H,W] 批次图像数据，N个图像，每个图像C个通道，每个图像的大小为H*W; 其中C必须是固定的
+        :return:
+        """
+        # 1. 卷积特征提取 [N,C,H,W] --> [N,64,8,8]
+        x = self.features(x)
+
+        # 2. 扁平化 [N,64,8,8] --> [N,64*8*8]
+        x = x.flatten(1, -1)
+
+        # 3. 全连接 决策判断 得到每个样本属于各个类别的置信度 [N,num_classes]
+        score = self.classify(x)
+        return score
+
+class VGGImageClassifyNetwork(nn.Module):
+    def __init__(self, num_classes, in_channels=3):
+        super().__init__()
+        # 将训练好的vgg模型参数作为我们当前任务的初始参数  -- 模型参数的迁移
+        self.vgg = models.vgg16_bn(weights=models.VGG16_BN_Weights.DEFAULT)
+        del self.vgg.classifier[-1] # 删除最后一个全连接操作
+
+        # 针对迁移的模型这部分的参数来讲，模型参数的更新有以下几种方式：
+        # 方式1：不区分迁移模块的参数和非迁移模块的参数，共同更新
+
+        # 方式2：针对迁移模块的参数不进行参数更新 --> 模型参数冻结
+        # 2.1 冻结所有迁移模块参数
+        # for name, param in self.vgg.named_parameters():
+        #     print(f"冻结参数:{name}")
+        #     param.requires_grad = False
+        # 2.2 冻结部分迁移模块参数(一般冻结前面的层)
+        # pass
+
+        # 方式3： 针对迁移部分的参数仅进行微调(eg：学习率调整成非迁移模块学习的1/100, 一般学习率至少1e-4以下)
+        # pass
+
+        self.classify = nn.Linear(in_features=4096, out_features=num_classes)
+
+    def forward(self, image):
+        # 1. 提取vgg特征
+        features = self.vgg(image)
+        # 2. 决策得到置信度
+        score = self.classify(features)
+        return score
+
+def build_network(**kwargs):
+    return VGGImageClassifyNetwork(
+        num_classes=kwargs['num_classes'],
+        in_channels=kwargs['in_channels']
+    )
