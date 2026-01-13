c0fd7ca4925571e7aa4aeeabb4b674c7404e6dfc
xing <52873782+LZX842056112@users.noreply.github.com>
2026-01-13 19:26:58 +0800

    code_1104_

--- code1104/01_FasterRCNN API理解.py
+++ code1104/01_FasterRCNN API理解.py
@@ -15,18 +15,22 @@ def t0():
     print(net)
 
 
+# 禁用梯度计算
 @torch.no_grad()
 def interface():
     ts = transforms.Compose([
         transforms.ToTensor()
     ])
     net = models.detection.fasterrcnn_resnet50_fpn(
+        # 加载预训练权重 ， COCO 预训练权重
         weights=models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT,
+        # 检测的阈值，低于这个阈值的边框会被忽略
         box_score_thresh=0.3
     )
     net.eval()
 
     images = [
+        # 每个图像张量形状为 [C, H, W] (通道数, 高度, 宽度)
         torch.rand(3, 300, 400),
         ts(Image.open("cat.jpg").convert("RGB")),
         ts(Image.open("dog.jpeg").convert("RGB"))
@@ -49,11 +53,14 @@ def training():
         box_score_thresh=0.3
     )
 
+    # images 是一个list，内部是[C,H,W]结构的tensor对象，每个tensor的H、W可以是不一样的，list中的tensor数量就是图像image_number
     images = [
         torch.rand(3, 300, 400),
         ts(Image.open("cat.jpg").convert("RGB")),
         ts(Image.open("dog.jpeg").convert("RGB"))
     ]
+    # targets 是一个list，内部就是每个图像对应的目标属性信息dict对象，也就是len(images) == len(targets);
+    # 包含真实标注的列表
     targets = [
         {
             'boxes': torch.tensor([]).reshape((0, 4)),
@@ -70,8 +77,6 @@ def training():
             'labels': torch.tensor([17, 67, 62])
         }
     ]
-    # images 是一个list，内部是[C,H,W]结构的tensor对象，每个tensor的H、W可以是不一样的，list中的tensor数量就是图像image_number
-    # targets 是一个list，内部就是每个图像对应的目标属性信息dict对象，也就是len(images) == len(targets);
     # ### targets中每个dict中包含两个值，boxes和labels，M表示当前图像中真实边框的数量
     # ### boxes 给定当前图像的真实边框坐标tensor, shape形状为:[M,4], 坐标顺序[x1,y1,x2,y2]
     # ### labels 给定图像的真实边框类别id tensor，shape形状为: [M]
